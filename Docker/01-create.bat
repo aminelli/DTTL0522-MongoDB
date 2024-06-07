1fb79f2e6c424524ad22da44ee71d53bbb143e9e
aminelli <56305052+aminelli@users.noreply.github.com>
2024-06-07 12:25:58 +0200

    dsaf

--- Docker/01-create.bat
+++ Docker/01-create.bat
@@ -15,3 +15,4 @@ REM docker volume create vol-mongodb-data-test
 REM docker volume create vol-mongodb-config-test
 REM docker run -d --name mongodbtest -h mongodbtest --network net-mongo -p 27018:27017 -v vol-mongodb-test:/data/db -v vol-mongodb-config-test:/data/configdb -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=Corso2024 --restart=always mongo
 
+
