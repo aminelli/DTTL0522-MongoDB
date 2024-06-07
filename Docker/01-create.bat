17af39e3560130ea3fb195fc62b285a849a1bfa8
aminelli <56305052+aminelli@users.noreply.github.com>
2024-06-07 12:10:21 +0200

    assd

--- Docker/01-create.bat
+++ Docker/01-create.bat
@@ -4,6 +4,12 @@ docker volume create vol-mongodb-data
 docker volume create vol-mongodb-config
 docker run -d --name mongodb -h mongodb --network net-mongo -p 27017:27017 -v vol-mongodb:/data/db -v vol-mongodb-config:/data/configdb -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=Corso2024 --restart=always mongo
 
+docker logs -f mongodb
+
+REM usr : root
+REM pwd : Corso2024
+
+
 REM EVENTUALE SECONDO CONTAINER
 REM docker volume create vol-mongodb-data-test
 REM docker volume create vol-mongodb-config-test
