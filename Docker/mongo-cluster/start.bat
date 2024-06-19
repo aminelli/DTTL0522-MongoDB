4afb5f583b27281389ad0cb553abdc2300f246ef
aminelli <56305052+aminelli@users.noreply.github.com>
2024-06-19 09:46:31 +0200

    asda

--- Docker/mongo-cluster/start.bat
+++ Docker/mongo-cluster/start.bat
@@ -2,8 +2,8 @@
 
 docker network create net-mongo-cluster
 
-docker run -d -p 27117:27017 --name mongo1 --network net-mongo-cluster mongo mongod --replSet repMongoCorso --bind_ip 0.0.0.0,mongo1
+docker run -d -p 27117:27017 --name mongo1 --network net-mongo-cluster mongo mongod --replSet repMongoCorso --bind_ip localhost,mongo1
 
-docker run -d -p 27118:27017 --name mongo2 --network net-mongo-cluster mongo mongod --replSet repMongoCorso --bind_ip 0.0.0.0,mongo2
+docker run -d -p 27118:27017 --name mongo2 --network net-mongo-cluster mongo mongod --replSet repMongoCorso --bind_ip localhost,mongo2
 
-docker run -d -p 27119:27017 --name mongo3 --network net-mongo-cluster mongo mongod --replSet repMongoCorso --bind_ip 0.0.0.0,mongo3
+docker run -d -p 27119:27017 --name mongo3 --network net-mongo-cluster mongo mongod --replSet repMongoCorso --bind_ip localhost,mongo3
