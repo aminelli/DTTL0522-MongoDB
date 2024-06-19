8fd6fc1244b1310563ec5bd00b8efe844e5d58a7
aminelli <56305052+aminelli@users.noreply.github.com>
2024-06-19 16:29:00 +0200

    asdfa

--- Scripts/07-coll-clustered/E001.mongodd.js
+++ Scripts/07-coll-clustered/E001.mongodd.js
@@ -4,7 +4,7 @@ use('dbprova');
 db.runCommand( {
   create: "products",
   clusteredIndex: { "key": { _id: 1 }, "unique": true, "name": "products clustered key" }
-} )
+})
 
 
 // Creazione di una clustered collection
@@ -12,6 +12,7 @@ db.createCollection(
   "stocks",
   { clusteredIndex: { "key": { _id: 1 }, "unique": true, "name": "stocks clustered key" } }
 )
+
 
 
 // Creazione di una clustered collection
@@ -24,10 +25,38 @@ db.createCollection(
 db.orders.insertMany( [
   { _id: ISODate( "2022-03-18T12:45:20Z" ), "quantity": 50, "totalOrderPrice": 500 },
   { _id: ISODate( "2022-03-18T12:47:00Z" ), "quantity": 5, "totalOrderPrice": 50 },
-  { _id: ISODate( "2022-03-18T12:50:00Z" ), "quantity": 1, "totalOrderPrice": 10 }
-] )
+  { _id: ISODate( "2022-03-18T12:50:00Z" ), "quantity": 1, "totalOrderPrice": 10 },
+])
 
 
 db.orders.find( { _id: { $gt: ISODate( "2022-03-18T12:47:00.000Z" ) } } )
 
 db.runCommand( { listCollections: 1 } )
+
+sh.status()
+
+// sh.enableSharding()
+// sh.shardCollection()
+
+// Shard collection con strategia hash
+sh.shardCollection("dbprova.testshard", { _id : "hashed" } )
+
+//
+sh.shardCollection("dbprova.testshard", { _id : "hashed" } )
+
+db.testshard.insertMany( [
+  { _id: ISODate( "2022-03-18T12:45:20Z" ), "quantity": 50, "totalOrderPrice": 500 },
+  { _id: ISODate( "2022-03-18T12:47:00Z" ), "quantity": 5, "totalOrderPrice": 50 },
+  { _id: ISODate( "2022-03-18T12:50:00Z" ), "quantity": 1, "totalOrderPrice": 10 },
+])
+
+sh.enableSharding("dbprova")+
+sh.shardCollection("dbprova.testshard2", { _id : 1 } )
+sh.splitAt("dbprova.testshard2", { _id : ISODate( "2022-01-01T00:00:00Z" ) } )
+sh.splitAt("dbprova.testshard2", { _id : ISODate( "2023-01-01T00:00:00Z" ) } )
+sh.splitAt("dbprova.testshard2", { _id : ISODate( "2024-01-01T00:00:00Z" ) } )
+db.testshard2.insertMany( [
+  { _id: ISODate( "2022-03-18T12:45:20Z" ), "quantity": 50, "totalOrderPrice": 500 },
+  { _id: ISODate( "2023-03-18T12:47:00Z" ), "quantity": 5, "totalOrderPrice": 50 },
+  { _id: ISODate( "2024-03-18T12:50:00Z" ), "quantity": 1, "totalOrderPrice": 10 },
+])
