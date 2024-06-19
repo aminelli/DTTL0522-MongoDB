93dd4687f9073b57a6d5aa1628cba79e17ecceac
aminelli <56305052+aminelli@users.noreply.github.com>
2024-06-19 10:01:12 +0200

    ads

--- Docker/mongo-cluster/start.bat
+++ Docker/mongo-cluster/start.bat
@@ -7,3 +7,6 @@ docker run -d -p 27117:27017 --name mongo1 --network net-mongo-cluster mongo mon
 docker run -d -p 27118:27017 --name mongo2 --network net-mongo-cluster mongo mongod --replSet repMongoCorso --bind_ip localhost,mongo2
 
 docker run -d -p 27119:27017 --name mongo3 --network net-mongo-cluster mongo mongod --replSet repMongoCorso --bind_ip localhost,mongo3
+
+
+docker exec -it mongo1 mongosh --eval "rs.initiate({ _id: \"repMongoCorso\", members: [ { _id: 0, host: \"mongo1\" }, { _id: 1, host: \"mongo2\" }, { _id: 2, host: \"mongo3\" } ]})"
